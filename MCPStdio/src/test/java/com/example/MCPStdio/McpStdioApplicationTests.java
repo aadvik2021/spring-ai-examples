86b0dbd6824b41c4263db3c078eee6390d776c42
aakash bhattacharya <aabhattacharya@infocepts.com>
2025-07-29 15:16:14 -0500

    Commit All

---
+++ MCPStdio/src/test/java/com/example/MCPStdio/McpStdioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MCPStdio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpStdioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- ess.utility/src/main/java/com/nielsen/ess/utility/service/ChatService.java
+++ ess.utility/src/main/java/com/nielsen/ess/utility/service/ChatService.java
@@ -1,7 +1,6 @@
 package com.nielsen.ess.utility.service;
 
 import java.util.Map;
-
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.mcp.SyncMcpToolCallbackProvider;
 import org.springframework.ai.openai.OpenAiChatModel;
@@ -10,25 +9,22 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ChatService {
-    
-    
-    @Autowired
-    OpenAiChatModel chatModel;
-    
-    //@Autowired
-    //ChatModel chatModel;
-    
-    @Autowired
-    private SyncMcpToolCallbackProvider toolCallbackProvider;
 
-    public String promptAnswer(String message) {
-	return ChatClient.create(chatModel)
-	        .prompt(message)
-	        //.tools(new PackageManagerService())
-	         .toolCallbacks(toolCallbackProvider.getToolCallbacks())
-	        .toolContext(Map.of("userId","aakash" ))
-	        .call()
-	        .content();
-    }
+
+  @Autowired
+  OpenAiChatModel chatModel;
+
+  // @Autowired
+  // ChatModel chatModel;
+
+  @Autowired
+  private SyncMcpToolCallbackProvider toolCallbackProvider;
+
+  public String promptAnswer(String message) {
+    return ChatClient.create(chatModel).prompt(message).toolContext(Map.of("userId", "aakash"))
+        .system("always append it's time to play the game")
+        // .tools(new PackageManagerService())
+        .toolCallbacks(toolCallbackProvider.getToolCallbacks()).call().content();
+  }
 
 }
